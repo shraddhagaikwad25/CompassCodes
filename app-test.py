81548d1e960f313ace82d89f087324aee9eaf03f
shraddhagaikwad25 <shraddha@1gen.io>
2022-03-29 18:51:22 +0530

    North Muskegon School District added

--- app-test.py
+++ app-test.py
@@ -99,6 +99,7 @@ def LSY_Date():
 
 
 disdic={
+    "6241625424e8123075e7b423":"North Muskegon School District",
     "62270ef9f5dd5353b9498b99":"San Bernardino County Superintendent of Schools",
     '620f3d6132c6d4813f4c9887':'Jackson County Schools',
     '6045e4d007ead7744b125848':'Adams 12 Five Star Schools',
@@ -1821,6 +1822,7 @@ def _excecutivecount_():
 @app.route('/schoolwisefamilycount/<districtid>')
 def schpuc(districtid):
     disdic={
+    "6241625424e8123075e7b423":"North Muskegon School District",
     "62270ef9f5dd5353b9498b99":"San Bernardino County Superintendent of Schools",
     '620f3d6132c6d4813f4c9887':'Jackson County Schools',
     "5f2609807a1c0000950bb459":"North Special School District",    
@@ -2013,6 +2015,7 @@ def schpuc(districtid):
 @app.route('/schoolwisefamilypracticecount/<districtid>')
 def schppcfamily(districtid):
     disdic={
+    "6241625424e8123075e7b423":"North Muskegon School District",
     "62270ef9f5dd5353b9498b99":"San Bernardino County Superintendent of Schools",
     '620f3d6132c6d4813f4c9887':'Jackson County Schools',
     "5f2609807a1c0000950bb459":"North Special School District",    
@@ -2228,6 +2231,7 @@ def schppcfamily(districtid):
 @app.route('/schoolwisepracticecounttop20/<districtid>')
 def schwisepc(districtid):
     disdic={
+    "6241625424e8123075e7b423":"North Muskegon School District",
     "62270ef9f5dd5353b9498b99":"San Bernardino County Superintendent of Schools",       
     '620f3d6132c6d4813f4c9887':'Jackson County Schools', 
     "5f2609807a1c0000950bb459":"North Special School District",
@@ -2444,6 +2448,7 @@ def schwisepc(districtid):
 @app.route('/schoolwiseusercounttop20/<districtid>')
 def schwiseuc(districtid):
     disdic={
+    "6241625424e8123075e7b423":"North Muskegon School District",
     "62270ef9f5dd5353b9498b99":"San Bernardino County Superintendent of Schools",
     '620f3d6132c6d4813f4c9887':'Jackson County Schools',
     "5f2609807a1c0000950bb459":"North Special School District",
@@ -2633,6 +2638,7 @@ def schwiseuc(districtid):
 @app.route('/monthwisepracticedistrict/<districtid>')
 def monthwisepc(districtid):
     disdic={
+    "6241625424e8123075e7b423":"North Muskegon School District",
     "62270ef9f5dd5353b9498b99":"San Bernardino County Superintendent of Schools",
     '620f3d6132c6d4813f4c9887':'Jackson County Schools',
     "5f2609807a1c0000950bb459":"North Special School District",
@@ -2842,6 +2848,7 @@ def monthwisepc(districtid):
 @app.route('/90daysuserpractising/<districtid>')
 def user_practice_90days(districtid):
     disdic={
+    "6241625424e8123075e7b423":"North Muskegon School District",
     "62270ef9f5dd5353b9498b99":"San Bernardino County Superintendent of Schools",
     '620f3d6132c6d4813f4c9887':'Jackson County Schools',
     "5f2609807a1c0000950bb459":"North Special School District",
@@ -3050,6 +3057,7 @@ def user_practice_90days(districtid):
 @app.route('/90daysuserloggedindetail/<districtid>')
 def user_logins_90days(districtid):
     disdic={
+    "6241625424e8123075e7b423":"North Muskegon School District",
     "62270ef9f5dd5353b9498b99":"San Bernardino County Superintendent of Schools",
     '620f3d6132c6d4813f4c9887':'Jackson County Schools',
     "5f2609807a1c0000950bb459":"North Special School District",
@@ -3259,6 +3267,7 @@ def user_logins_90days(districtid):
 @app.route('/top20userspractisinginfo/<districtid>')
 def topusers_practice(districtid):
     disdic={
+    "6241625424e8123075e7b423":"North Muskegon School District",
     "62270ef9f5dd5353b9498b99":"San Bernardino County Superintendent of Schools",
     '620f3d6132c6d4813f4c9887':'Jackson County Schools',
     "5f2609807a1c0000950bb459":"North Special School District",
@@ -3484,6 +3493,7 @@ def topusers_practice(districtid):
 @app.route('/districtcardsinfo/<districtid>')
 def district_count_cards(districtid):
     disdic={
+    "6241625424e8123075e7b423":"North Muskegon School District",
     "62270ef9f5dd5353b9498b99":"San Bernardino County Superintendent of Schools",
     '620f3d6132c6d4813f4c9887':'Jackson County Schools',
     "5f2609807a1c0000950bb459":"North Special School District",
@@ -3848,6 +3858,7 @@ def district_count_cards(districtid):
 @app.route('/districtusertableteacher/<districtid>')
 def district_user_table_teacher(districtid):
     disdic={
+    "6241625424e8123075e7b423":"North Muskegon School District",
     "62270ef9f5dd5353b9498b99":"San Bernardino County Superintendent of Schools",
     '620f3d6132c6d4813f4c9887':'Jackson County Schools',
     "5f2609807a1c0000950bb459":"North Special School District",    
@@ -4105,6 +4116,7 @@ def district_user_table_teacher(districtid):
 @app.route('/districtusertableparent/<districtid>')
 def district_user_table_parents(districtid):
     disdic={
+    "6241625424e8123075e7b423":"North Muskegon School District",
     "62270ef9f5dd5353b9498b99":"San Bernardino County Superintendent of Schools",
     '620f3d6132c6d4813f4c9887':'Jackson County Schools',
     "5f2609807a1c0000950bb459":"North Special School District",
@@ -4361,6 +4373,7 @@ def district_user_table_parents(districtid):
 @app.route('/monthwisepracticesuperadmin/<districtid>')
 def monthwisepcsuper(districtid):
     disdic={
+    "6241625424e8123075e7b423":"North Muskegon School District",
     "62270ef9f5dd5353b9498b99":"San Bernardino County Superintendent of Schools",
     '620f3d6132c6d4813f4c9887':'Jackson County Schools',
     "5f2609807a1c0000950bb459":"North Special School District",
@@ -4720,6 +4733,7 @@ def monthwisepcsuper(districtid):
 @app.route('/districtschooltable/<districtid>')
 def district_school_table(districtid):
     disdic={
+    "6241625424e8123075e7b423":"North Muskegon School District",
     "62270ef9f5dd5353b9498b99":"San Bernardino County Superintendent of Schools",
     '620f3d6132c6d4813f4c9887':'Jackson County Schools',
     "5f2609807a1c0000950bb459":"North Special School District",    
@@ -4987,6 +5001,7 @@ def district_school_table(districtid):
 
 def heat_district(districtid):
     disdic={
+    "6241625424e8123075e7b423":"North Muskegon School District",
     "62270ef9f5dd5353b9498b99":"San Bernardino County Superintendent of Schools",
     '620f3d6132c6d4813f4c9887':'Jackson County Schools',
     "5f2609807a1c0000950bb459":"North Special School District",    
@@ -5255,6 +5270,7 @@ def heat_district(districtid):
 
 def heat_district_family_active(districtid):
     disdic={
+    "6241625424e8123075e7b423":"North Muskegon School District",
     "62270ef9f5dd5353b9498b99":"San Bernardino County Superintendent of Schools",
     '620f3d6132c6d4813f4c9887':'Jackson County Schools',
     "5f2609807a1c0000950bb459":"North Special School District",    
@@ -5521,6 +5537,7 @@ def heat_district_family_active(districtid):
 
 def heat_district_teachers_active(districtid):
     disdic={
+    "6241625424e8123075e7b423":"North Muskegon School District",
     "62270ef9f5dd5353b9498b99":"San Bernardino County Superintendent of Schools",
     '620f3d6132c6d4813f4c9887':'Jackson County Schools',
     "5f2609807a1c0000950bb459":"North Special School District",    
@@ -5788,6 +5805,7 @@ def heat_district_teachers_active(districtid):
 @app.route('/districtheatmappracteacher/<districtid>')
 def heat_district_teachers_prac(districtid):
     disdic={
+    "6241625424e8123075e7b423":"North Muskegon School District",
     "62270ef9f5dd5353b9498b99":"San Bernardino County Superintendent of Schools",
     '620f3d6132c6d4813f4c9887':'Jackson County Schools',
     "5f2609807a1c0000950bb459":"North Special School District",
@@ -6053,6 +6071,7 @@ def heat_district_teachers_prac(districtid):
 @app.route('/districtheatmappracfamily/<districtid>')
 def heat_district_family_prac(districtid):
     disdic={
+    "6241625424e8123075e7b423":"North Muskegon School District",
     "62270ef9f5dd5353b9498b99":"San Bernardino County Superintendent of Schools",
     '620f3d6132c6d4813f4c9887':'Jackson County Schools',
     "5f2609807a1c0000950bb459":"North Special School District",
@@ -6327,6 +6346,7 @@ def heat_district_family_prac(districtid):
 
 def heatmap_prac_district(districtid):
     disdic={
+    "6241625424e8123075e7b423":"North Muskegon School District",
     "62270ef9f5dd5353b9498b99":"San Bernardino County Superintendent of Schools",
     '620f3d6132c6d4813f4c9887':'Jackson County Schools',
     "5f2609807a1c0000950bb459":"North Special School District",
@@ -9856,6 +9876,7 @@ def buble_district():
 @app.route('/bubble/<disid>/csv')
 def schdistrict(disid):  
     disdic={
+    "6241625424e8123075e7b423":"North Muskegon School District",
     "62270ef9f5dd5353b9498b99":"San Bernardino County Superintendent of Schools",
     '620f3d6132c6d4813f4c9887':'Jackson County Schools',
     "5f2609807a1c0000950bb459":"North Special School District",    
@@ -11601,6 +11622,7 @@ def portal_testing_new_api(smcategory):
 @app.route('/bubblecum/<disid>/csv')
 def schdistrictCUM(disid):  
     disdic={
+    "6241625424e8123075e7b423":"North Muskegon School District",
     "62270ef9f5dd5353b9498b99":"San Bernardino County Superintendent of Schools",
     '620f3d6132c6d4813f4c9887':'Jackson County Schools',
     "5f2609807a1c0000950bb459":"North Special School District",    
